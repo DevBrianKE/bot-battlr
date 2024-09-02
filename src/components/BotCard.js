6aeb8743ec53d6a37109d72341bc400039fe3d32
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-09-03 00:40:21 +0300

    Enhanced BotCard component with icons and dynamic content based on bot properties

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Mapping of bot types to corresponding icon classes
 const botTypeClasses = {
   Assault: "icon military",
   Defender: "icon shield",
@@ -9,49 +10,50 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot }) {
+function BotCard({ bot, onAddBot }) {
   return (
     <div className="ui column">
+      {/* Card container for each bot */}
       <div
         className="ui card"
-        key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        key={bot.id} // Unique key for each card (can be omitted here as it's already used in parent)
+        onClick={onAddBot} // Event handler for clicking the card
       >
         <div className="image">
-          <img alt="oh no!" src={bot.avatar_url} />
+          <img alt="Bot Avatar" src={bot.avatar_url} /> {/* Image of the bot */}
         </div>
         <div className="content">
           <div className="header">
             {bot.name}
-            <i className={botTypeClasses[bot.bot_class]} />
+            <i className={botTypeClasses[bot.bot_class]} /> {/* Icon for bot type */}
           </div>
           <div className="meta text-wrap">
-            <small>{bot.catchphrase}</small>
+            <small>{bot.catchphrase}</small> {/* Catchphrase of the bot */}
           </div>
         </div>
         <div className="extra content">
           <span>
-            <i className="icon heartbeat" />
+            <i className="icon heartbeat" /> {/* Health icon */}
             {bot.health}
           </span>
-
           <span>
-            <i className="icon lightning" />
+            <i className="icon lightning" /> {/* Damage icon */}
             {bot.damage}
           </span>
           <span>
-            <i className="icon shield" />
+            <i className="icon shield" /> {/* Armor icon */}
             {bot.armor}
           </span>
           <span>
             <div className="ui center aligned segment basic">
               <button
                 className="ui mini red button"
-                onClick={() =>
-                  console.log("add code to connect event listener")
-                }
+                onClick={(e) => {
+                  e.stopPropagation(); // Prevents click event from bubbling up
+                  onAddBot(); // Calls the function to add the bot
+                }}
               >
-                x
+                x {/* Button to remove or add the bot */}
               </button>
             </div>
           </span>
