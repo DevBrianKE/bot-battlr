2ca0366a627bc1b4777670e68fe0af9d8db90e43
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-09-03 00:35:22 +0300

    Added BotCard component with bot type icons and event handling

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,18 @@
 import React from "react";
+import BotCard from "./BotCard"; // Importing BotCard component
 
-function BotCollection() {
-  // Your code here
+function BotCollection({ bots, onAddBot }) { // Added props: bots and onAddBot
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {bots.map(bot => (
+          // Map over the bots array to create a BotCard for each bot
+          <BotCard
+            key={bot.id} // Unique key for each BotCard to help React identify which items have changed
+            bot={bot} 
+            onAddBot={() => onAddBot(bot)} // Pass a function to handle adding the bot
+          />
+        ))}
       </div>
     </div>
   );
